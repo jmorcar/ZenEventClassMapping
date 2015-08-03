4101bf0265bb84761459f24c4bfc17a4b5e02a5b
Juan Manuel Moreno Carballo <juan_manuel_mc@hotmail.com>
2015-08-03 19:28:09 +0200

    Created addEventClassFromCSV

---
+++ addEventClassFromCSV/add_EventClassMapping_from_csv.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+import Globals, sys, ast, yaml, re
+from transaction import commit
+from Products.ZenUtils.ZenScriptBase import ZenScriptBase
+
+
+
+
+
+if len(sys.argv) < 2:
+     sys.exit('\nError faltan argumentos\n\nUsage: %s "fichero_CSV"\n\n' % sys.argv[0])
+
+
+
+def evalSeverity(severity):
+    return {
+        'Clear': '1',
+        'Normal': '2',
+		'Warning': '2',
+        'Minor': '3',
+		'Major': '4',
+        'Critical': '5',
+    }.get(severity, 2)
+
+def evalLogonly(logonly):
+		if re.match("LOGONLY", logonly):
+			return True
+		else:
+			return False
+
+
+#dmd = ZenScriptBase(connect=True).dmd
+##########################################
+# CONEXION CON ZOPE
+conex = None
+dmd = None
+try:
+    dmd = ZenScriptBase(connect=True).dmd
+except Exception, e:
+    print "Connection to zenoss dmd failed: %s\n" % e
+    sys.exit(1)
+###########################################
+			
+
+
+event_classes = '/Events/NUEVOS'
+# PROVISION DE NUEVAS EVENTCLASSES EN ZENOSS
+print "+ ADDING Event Class %s" % event_classes
+conex = dmd.Events.getOrganizer('/')
+conex.dmd.Events.manage_addOrganizer(event_classes)
+
+	
+	 
+	 
+#CARGAMOS EL FICHERO
+file = sys.argv[1]	 
+datos = open(file)
+try:
+		
+        for line in datos:
+				line_split = line.split(";" , 6)
+				trap_name = line_split[0]
+				oid = line_split[1]
+				print (line_split[2])
+				logonly = evalLogonly(line_split[2])
+				severidad = evalSeverity(line_split[4])
+				texto_trap = line_split[5]
+				mytransform = """ 
+						evt.serverity = %s
+						evt.summary = %s
+						""" % (severidad , texto_trap)
+				if logonly:
+					mytransform += """
+					
+							evt._action = "history"
+							"""
+				
+				print ("Creando instancia -> %s" % trap_name)
+				print (mytransform)
+				inst = conex.find(trap_name)
+				#inst.transform = mytransform
+				print(inst)
+				
+				
+				
+				
+				
+#CERRAR FICHERO
+finally:
+        datos.close()	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+
+
+
+#commit()
